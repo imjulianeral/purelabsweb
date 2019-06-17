24c8ad611473b5746a8b00f6e1c5cb24afc7461e
Jorge <julianacero98@gmail.com>
2019-06-16 21:53:35 -0500

    paths updated

---
+++ precache-manifest.5216a572dbe7bb1f8b87f70697794dbd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11956bfecd9f1682e8c977416a8d540",
+    "url": "/purelabsweb/index.html"
+  },
+  {
+    "revision": "8633f013410f2f8c2646",
+    "url": "/purelabsweb/static/css/main.7e6ab60d.chunk.css"
+  },
+  {
+    "revision": "40f2f863550f505544c5",
+    "url": "/purelabsweb/static/js/2.59e50447.chunk.js"
+  },
+  {
+    "revision": "8633f013410f2f8c2646",
+    "url": "/purelabsweb/static/js/main.678d4907.chunk.js"
+  },
+  {
+    "revision": "9fe3d27ef0684afbb31e",
+    "url": "/purelabsweb/static/js/runtime~main.313e3586.js"
+  },
+  {
+    "revision": "6b4417099d963b005b2a4fa5fe13c4fc",
+    "url": "/purelabsweb/static/media/logo.6b441709.png"
+  },
+  {
+    "revision": "b6aac6274ee1d86f7fed1a8ee6f4a68e",
+    "url": "/purelabsweb/static/media/mobiledev.b6aac627.jpg"
+  },
+  {
+    "revision": "0bd05e78ea42ae2351270c1c53f5ce1b",
+    "url": "/purelabsweb/static/media/softstore.0bd05e78.png"
+  },
+  {
+    "revision": "d3f2531e50497f4c8ee31effb5eb3be3",
+    "url": "/purelabsweb/static/media/vision.d3f2531e.png"
+  },
+  {
+    "revision": "4777de9e8e1419846bb5dcb8d49a801b",
+    "url": "/purelabsweb/static/media/webdev.4777de9e.png"
+  }
+]);
