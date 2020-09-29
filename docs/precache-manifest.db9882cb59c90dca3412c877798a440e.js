7e7aab8ebfbef8ad565638dbd06d25a9e145ea12
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2020-09-29 17:03:41 -0500

    gh-pages

---
+++ docs/precache-manifest.db9882cb59c90dca3412c877798a440e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cc54b06d59887e4338e1686b88ab45",
+    "url": "/purelabsweb/index.html"
+  },
+  {
+    "revision": "8084e3965e6e36e8414b",
+    "url": "/purelabsweb/static/css/main.7e6ab60d.chunk.css"
+  },
+  {
+    "revision": "40f2f863550f505544c5",
+    "url": "/purelabsweb/static/js/2.59e50447.chunk.js"
+  },
+  {
+    "revision": "8084e3965e6e36e8414b",
+    "url": "/purelabsweb/static/js/main.b6d4f8f9.chunk.js"
+  },
+  {
+    "revision": "9fe3d27ef0684afbb31e",
+    "url": "/purelabsweb/static/js/runtime~main.313e3586.js"
+  },
+  {
+    "revision": "6b4417099d963b005b2a4fa5fe13c4fc",
+    "url": "/purelabsweb/static/media/logo.6b441709.png"
+  },
+  {
+    "revision": "b6aac6274ee1d86f7fed1a8ee6f4a68e",
+    "url": "/purelabsweb/static/media/mobiledev.b6aac627.jpg"
+  },
+  {
+    "revision": "0bd05e78ea42ae2351270c1c53f5ce1b",
+    "url": "/purelabsweb/static/media/softstore.0bd05e78.png"
+  },
+  {
+    "revision": "d3f2531e50497f4c8ee31effb5eb3be3",
+    "url": "/purelabsweb/static/media/vision.d3f2531e.png"
+  },
+  {
+    "revision": "4777de9e8e1419846bb5dcb8d49a801b",
+    "url": "/purelabsweb/static/media/webdev.4777de9e.png"
+  }
+]);
